d77ea0fc7c41bd1c5d4212d41840e0473bb6f697
ISAH IBRAHIM <155283606+GenHibee@users.noreply.github.com>
2025-11-04 16:22:23 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -46,3 +46,10 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+//MENU TOGGLE
+const menuToggle = document.getElementById('menu-toggle');
+const navMenu = document.getElementById('nav-menu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
